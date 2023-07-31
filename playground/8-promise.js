f7b78b30142ddc8469275587252687e0a553580b
Prudhvi21 <dasariprudhvisai26@gmail.com>
2023-07-31 11:02:59 +0530

    Added mongoose,apis and tokens

--- playground/8-promise.js
+++ playground/8-promise.js
@@ -1,14 +1,33 @@
-const doWorkMyPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve([1, 2, 3, 4]);
-    reject("There is a Error!");
-  }, 1000);
-});
+// const doWorkMyPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve([1, 2, 3, 4]);
+//     reject("There is a Error!");
+//   }, 1000);
+// });
 
-doWorkMyPromise
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log("Error ", error);
+// doWorkMyPromise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log("Error ", error);
+//   });
+
+const add = (a, b) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(a + b);
+    }, 2000);
   });
+};
+
+add(1, 1).then((sum) => {
+  console.log(sum);
+  return add(sum, 4)
+    .then((sum2) => {
+      console.log(sum2);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+//Connecting to the database...
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// const me = new User({
+//   name: "Virat",
+//   email: "Virat@gmail.com",
+//   password: "virat181",
+//   age: 35,
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// const task = new Task({
+//   description: " Eat Lunch",
+// });
+
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
